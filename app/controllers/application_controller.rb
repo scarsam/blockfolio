2f933bed69dc782b8d2b738e8ce55a244a695c39
Sam Ojling <s.ojling@gmail.com>
2018-01-10 20:08:12 -0800

    Added session helpers, and conditionals for displaying login/logout link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
