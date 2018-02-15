67b0ab5fd224b3d049b82daf213f5703992a944c
Sam Ojling <s.ojling@gmail.com>
2018-02-15 14:33:52 +0100

    Remove show action that I don't use anymore

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,9 +1,3 @@
 class CoinsController < ApplicationController
-
-  def show
-    @coin = Coin.find(params[:id])
-    @portfolio = Portfolio.find(params[:portfolio_id])
-    @coin_portfolio = CoinPortfolio.find_by(portfolio_id: @portfolio.id, coin_id: @coin.id)
-  end
-
+  
 end
