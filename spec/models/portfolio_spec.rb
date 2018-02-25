e6e56d4c9f807e6e92cb55546d1359c64cba3c07
Sam Ojling <s.ojling@gmail.com>
2018-02-25 10:21:15 +0100

    Added specs for portfolio

--- spec/models/portfolio_spec.rb
+++ spec/models/portfolio_spec.rb
@@ -1,1 +1,41 @@
 require 'rails_helper'
+
+describe Portfolio, type: :model do
+  context "created with a name and user" do
+    it "should return valid" do
+      user = User.create(
+        email: 'sam@ojling.com',
+        password: 'test123'
+      )
+      portfolio = Portfolio.new(name: 'Sam Portfolio 3')
+      user.portfolios << portfolio
+      user.save
+      expect(portfolio).to be_valid
+    end
+  end
+  context "created without a name" do
+    it "should return invalid" do
+      user = User.create(
+        email: 'sam@ojling.com',
+        password: 'test123'
+      )
+      portfolio = Portfolio.new
+      user.portfolios << portfolio
+      user.save
+      expect(portfolio).to be_invalid
+    end
+  end
+  context "created two portfolios with the same name" do
+    it "should return invalid" do
+      user = User.create(
+        email: 'sam@ojling.com',
+        password: 'test123'
+      )
+      user.portfolios.create(name: 'Same name')
+      portfolio2 = Portfolio.new(name: 'Same name')
+      user.portfolios << portfolio2
+      user.save
+      expect(portfolio2).to be_invalid
+    end
+  end
+end
