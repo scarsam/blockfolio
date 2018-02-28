7748389705df1b5c915ae0ad8841b8f068c5bf6b
Sam Ojling <s.ojling@gmail.com>
2018-02-28 12:19:38 +0100

    Continue styling and change the UX

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,11 @@ module Blockfolio
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
   end
+end
+
+# Removes rails field_with_errors wrapper
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
 end
